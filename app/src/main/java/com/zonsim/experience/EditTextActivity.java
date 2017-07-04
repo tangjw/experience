4aa5e3868bbccc1afe4d8231c08a4ea6a7c660aa
tangjw <tjwabc@gmail.com>
2017-07-04 17:35:03 +0800

    Initial commit

---
+++ app/src/main/java/com/zonsim/experience/EditTextActivity.java
@@ -0,0 +1,25 @@
+package com.zonsim.experience;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+/**
+ * ^-^
+ * Created by tang-jw on 2017/7/4.
+ */
+
+public class EditTextActivity extends AppCompatActivity {
+    
+    private EditText mEditText;
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edittext);
+        
+        mEditText = (EditText) findViewById(R.id.et_text);
+        
+    }
+}
---
+++ app/src/main/java/com/zonsim/experience/widget/ClearEditText.java
@@ -0,0 +1,126 @@
+package com.zonsim.experience.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.TranslateAnimation;
+
+/**
+ * 自带清空按钮的EditText
+ * Created by tang-jw on 2017/7/4.
+ */
+
+public class ClearEditText extends AppCompatEditText implements
+        View.OnFocusChangeListener, TextWatcher {
+    
+    private Drawable mClearDrawable;
+    private boolean mHasFocus;
+    
+    public ClearEditText(Context context) {
+        this(context, null);
+        
+    }
+    
+    public ClearEditText(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.editTextStyle);
+    }
+    
+    public ClearEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        
+        init();
+        
+    }
+    
+    private void init() {
+        
+        mClearDrawable = getCompoundDrawables()[2];
+        if (mClearDrawable == null) {
+            mClearDrawable = getResources().getDrawable(android.R.drawable.ic_menu_close_clear_cancel);
+        }
+
+//        mClearDrawable.setBounds(0, 0, mClearDrawable.getIntrinsicWidth(), mClearDrawable.getIntrinsicHeight());
+        
+        
+        setOnFocusChangeListener(this);
+        addTextChangedListener(this);
+    }
+    
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(@DrawableRes int left, @DrawableRes int top, @DrawableRes int right, @DrawableRes int bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        setCompoundDrawablesWithIntrinsicBounds(null, null,
+                right != 0 ? getContext().getResources().getDrawable(right) : null, null);
+        
+    }
+    
+    public void setClearIconIsVisible(boolean isVisible) {
+        setCompoundDrawablesWithIntrinsicBounds(null, null, isVisible ? mClearDrawable : null, null);
+    }
+    
+    
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        
+    }
+    
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (mHasFocus) {
+            setClearIconIsVisible(s.length() > 0);
+        }
+    }
+    
+    @Override
+    public void afterTextChanged(Editable s) {
+        
+    }
+    
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        mHasFocus = hasFocus;
+        if (mHasFocus) {
+            setClearIconIsVisible(getText().length() > 0);
+        } else {
+            setClearIconIsVisible(false);
+        }
+    }
+    
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP && mClearDrawable != null) {
+            boolean isTouchClearIcon = event.getX() > (getWidth() - getTotalPaddingRight())
+                    && event.getX() < getWidth() - getPaddingRight();
+            if (isTouchClearIcon) {
+                setText(null);
+                shakeAnimation(5);
+            }
+            
+        }
+        
+        return super.onTouchEvent(event);
+    }
+    
+    
+    /**
+     * 晃动动画
+     *
+     * @param counts 1秒钟晃动多少下
+     * @return
+     */
+    public Animation shakeAnimation(int counts) {
+        Animation translateAnimation = new TranslateAnimation(0, 10, 0, 0);
+        translateAnimation.setInterpolator(new CycleInterpolator(counts));
+        translateAnimation.setDuration(1000);
+        return translateAnimation;
+    }
+}
